5c7419db36a687319cfeffd44e4439c6ce24c8b3
Concetto <info@cvsolutions.it>
2013-10-05 16:08:58 +0200

    update config

--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,10 @@
 <?php
 return array(
+	'service_manager'=>array(
+        'factories' => array(
+            'Slug' => 'ZfSnippets\Service\Slug'
+        ),
+    ),
     'view_helpers' => array(
         'invokables'=> array(
             'TruncateHtml' => 'ZfSnippets\View\Helper\TruncateHtml'   
