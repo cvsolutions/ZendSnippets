bbad07db9a837dffe13f57cdaece3aec78fa128e
Concetto <info@cvsolutions.it>
2013-10-05 16:02:09 +0200

    update config file

--- config/module.config.php
+++ config/module.config.php
@@ -1,34 +1,8 @@
 <?php
 return array(
-    'controllers' => array(
-        'invokables' => array(
-            'ZfSnippets\Controller\ZfSnippets' => 'ZfSnippets\Controller\ZfSnippetsController',
-        ),
-    ),
-    'router' => array(
-        'routes' => array(
-            'zfsnippets' => array(
-                'type'    => 'segment',
-                'options' => array(
-                    'route'    => '/zfsnippets[/:action][/:id]',
-                    'constraints' => array(
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id'     => '[0-9]+',
-                    ),
-                    'defaults' => array(
-                        'controller' => 'ZfSnippets\Controller\ZfSnippets',
-                        'action'     => 'index',
-                        'action'     => 'search',
-                        'action'     => 'recent',
-                    ),
-                ),
-            ),
-        ),
-    ),
-
-    'view_manager' => array(
-        'template_path_stack' => array(
-            'zfsnippets' => __DIR__ . '/../view',
-        ),
+    'view_helpers' => array(
+        'invokables'=> array(
+            'TruncateHtml' => 'ZfSnippets\View\Helper\TruncateHtml'   
+        )
     ),
 );
